6a371eb4e6a8b9b22a814712cf0c6cd3a9df5be4
Alfonso <alfonso.garcia2324@gmail.com>
2019-05-13 16:28:03 -0700

    Switched out setInterval for setTimeout

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -14,7 +14,7 @@ class Counter extends Component {
   incrementAsync = () => {
     // Stretch Problem: Implement an increment function that
     // increments after waiting for one second
-    setInterval(() => {
+    setTimeout(() => {
       this.props.increment();
     }, 1000);
   };
